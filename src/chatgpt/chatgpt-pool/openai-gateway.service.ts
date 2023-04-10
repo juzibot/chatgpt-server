441081d8fc724e09218ed5dec44e49d1f3c0ea73
windmemory <wind.memory.cn@gmail.com>
2023-04-11 01:37:52 +0800

    convert azure like stream response to be openai like stream response

--- src/chatgpt/chatgpt-pool/openai-gateway.service.ts
+++ src/chatgpt/chatgpt-pool/openai-gateway.service.ts
@@ -1,11 +1,12 @@
 import { HttpException, Inject, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
-import { Agent as HttpAgent } from 'http';
+import { Agent as HttpAgent, IncomingMessage } from 'http';
 import { Agent as HttpsAgent } from 'https';
 import { SocksProxyAgent } from 'socks-proxy-agent';
 import { MINUTE } from 'src/common/time';
 import { AccountType, ChatgptAccount } from 'src/entities/chatgpt-account';
+import { PassThrough } from 'stream';
 
 @Injectable()
 export class OpenAIGatewayService {
@@ -28,7 +29,9 @@ export class OpenAIGatewayService {
       default:
         throw new Error(`Unknown account type: ${type}`);
     }
-
+    // debug only, keep this for now, remove this after the feature getting stable
+    // let count = 0;
+    // result.on('data', data => console.log(`${count++}: ${data.toString()}`));
     this.logger.log(`${type} request took ${(Date.now() - startTime) / 1000}s`);
     return result;
   }
@@ -97,7 +100,11 @@ export class OpenAIGatewayService {
         const error = new HttpException(response.data, response.status);
         throw error;
       }
-      return response.data;
+      if (data.stream) {
+        return this.convertAzureStreamToOpenAIStream(response.data);
+      } else {
+        return response.data;
+      }
     } catch (e) {
       if (e.response) {
         throw new HttpException(e.response.data, e.response.status);
@@ -107,5 +114,44 @@ export class OpenAIGatewayService {
         throw e;
       }
     }
+  }
+
+  private convertAzureStreamToOpenAIStream (stream: IncomingMessage) {
+    const normalizedStream = new PassThrough();
+    let canvas = '\n\n';
+    stream.on('data', (data: Buffer) => {
+      const dataStr = data.toString();
+      canvas += dataStr;
+      const lines = canvas.split('\n\ndata: ').filter(line => !!line).map(line => line.trim());
+      let firstMsg = '';
+      for (const line of lines) {
+        if (line === '[DONE]') {
+          normalizedStream.write(Buffer.from(`data: ${line}`));
+          continue;
+        }
+        try {
+          const obj = JSON.parse(line);
+          if (obj?.choices[0]?.delta?.role === 'assistant') {
+            firstMsg = line;
+            continue;
+          }
+          if (firstMsg) {
+            normalizedStream.write(Buffer.from(`data: ${firstMsg}\n\ndata: ${line}\n\n`));
+            firstMsg = '';
+          } else {
+            normalizedStream.write(Buffer.from(`data: ${line}\n\n`));
+          }
+        } catch (e) {
+          this.logger.log(`Failed to parse line: ${line} (${e.message})`);
+          canvas = `\n\ndata: ${line}`;
+          continue;
+        }
+        canvas = '\n\n';
+      }
+    });
+    stream.on('end', () => normalizedStream.end());
+    stream.on('error', err => normalizedStream.emit('error', err));
+
+    return normalizedStream;
   }
 }
